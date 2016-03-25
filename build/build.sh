2a7cd96fa02dadab1c80a6a063c6cc9e1f98e275
kohkimakimoto <kohki.makimoto@gmail.com>
2016-03-25 12:07:47 +0900

    update build flag

--- build/build.sh
+++ build/build.sh
@@ -209,7 +209,7 @@ do_dev() {
 
     cd ..
     go build \
-        -ldflags="-w -X github.com/kohkimakimoto/${name}/${name}.CommitHash=`git log --pretty=format:%H -n 1`" \
+        -ldflags="-X github.com/kohkimakimoto/${name}/${name}.CommitHash=`git log --pretty=format:%H -n 1`" \
         -o="build/dev/${name}" \
         ./cmd/${name}/${name}.go
 
