e7a575c892337b9da13a79dd4c1261c181922a7b
kohkimakimoto <kohki.makimoto@gmail.com>
2016-04-26 06:16:03 +0900

    fix bug

--- modules/sshrc/index.lua
+++ modules/sshrc/index.lua
@@ -27,27 +27,27 @@ local sshrc = function(override_config)
         essh.debug("sshrc sshhome: ".. config.sshhome)
 
         local command_v = [=[
-            function _essh_sshrc() {
-                local SSHHOME=${SSHHOME:=]=] .. config.sshhome ..[=[}
-                if [ -f $SSHHOME/.sshrc ]; then
-                    local files=.sshrc
-                    if [ -d $SSHHOME/.sshrc.d ]; then
-                        files="$files .sshrc.d"
-                    fi
-                    SIZE=$(tar cz -h -C $SSHHOME $files | wc -c)
-                    if [ $SIZE -gt 65536 ]; then
-                        echo >&2 $'.sshrc.d and .sshrc files must be less than 64kb\ncurrent size: '$SIZE' bytes'
-                        exit 1
-                    fi
+function _essh_sshrc() {
+    local SSHHOME=${SSHHOME:=]=] .. config.sshhome ..[=[}
+    if [ -f $SSHHOME/.sshrc ]; then
+        local files=.sshrc
+        if [ -d $SSHHOME/.sshrc.d ]; then
+            files="$files .sshrc.d"
+        fi
+        SIZE=$(tar cz -h -C $SSHHOME $files | wc -c)
+        if [ $SIZE -gt 65536 ]; then
+            echo >&2 $'.sshrc.d and .sshrc files must be less than 64kb\ncurrent size: '$SIZE' bytes'
+            exit 1
+        fi
 
-                    # echo $(tar cz -h -C $SSHHOME $files | xxd -p)
-                else
-                    echo "No such file: $SSHHOME/.sshrc" >&2
-                    exit 1
-                fi
-            }
-            _essh_sshrc
-        ]=]
+        # echo $(tar cz -h -C $SSHHOME $files | xxd -p)
+    else
+        echo "No such file: $SSHHOME/.sshrc" >&2
+        exit 1
+    fi
+}
+_essh_sshrc
+]=]
         -- validation
         local status = os.execute(command_v)
         if status ~= 0 then
@@ -55,27 +55,27 @@ local sshrc = function(override_config)
         end
 
         local command = [=[
-            function _essh_sshrc() {
-                local SSHHOME=${SSHHOME:=]=] .. config.sshhome ..[=[}
-                if [ -f $SSHHOME/.sshrc ]; then
-                    local files=.sshrc
-                    if [ -d $SSHHOME/.sshrc.d ]; then
-                        files="$files .sshrc.d"
-                    fi
-                    SIZE=$(tar cz -h -C $SSHHOME $files | wc -c)
-                    if [ $SIZE -gt 65536 ]; then
-                        echo >&2 $'.sshrc.d and .sshrc files must be less than 64kb\ncurrent size: '$SIZE' bytes'
-                        exit 1
-                    fi
+function _essh_sshrc() {
+    local SSHHOME=${SSHHOME:=]=] .. config.sshhome ..[=[}
+    if [ -f $SSHHOME/.sshrc ]; then
+        local files=.sshrc
+        if [ -d $SSHHOME/.sshrc.d ]; then
+            files="$files .sshrc.d"
+        fi
+        SIZE=$(tar cz -h -C $SSHHOME $files | wc -c)
+        if [ $SIZE -gt 65536 ]; then
+            echo >&2 $'.sshrc.d and .sshrc files must be less than 64kb\ncurrent size: '$SIZE' bytes'
+            exit 1
+        fi
 
-                    echo $(tar cz -h -C $SSHHOME $files | xxd -p)
-                else
-                    echo "No such file: $SSHHOME/.sshrc" >&2
-                    exit 1
-                fi
-            }
-            _essh_sshrc
-        ]=]
+        echo $(tar cz -h -C $SSHHOME $files | xxd -p)
+    else
+        echo "No such file: $SSHHOME/.sshrc" >&2
+        exit 1
+    fi
+}
+_essh_sshrc
+]=]
 
         local f = io.popen(command)
         local result = f:read("*a")
@@ -90,27 +90,27 @@ local sshrc = function(override_config)
         }
 
         local severside_script = template.dostring([=[
-            command -v xxd >/dev/null 2>&1 || { echo >&2 "sshrc requires xxd to be installed on the server, but it's not. Aborting."; exit 1; }
-            if [ -e /etc/motd ]; then cat /etc/motd; fi
-            if [ -e /etc/update-motd.d ]; then run-parts /etc/update-motd.d/ 2>/dev/null; fi
-            export SSHHOME=$(mktemp -d -t .$(whoami).sshrc.XXXX)
-            export SSHRCCLEANUP=$SSHHOME
-            trap "rm -rf $SSHRCCLEANUP; exit" 0
+command -v xxd >/dev/null 2>&1 || { echo >&2 "sshrc requires xxd to be installed on the server, but it's not. Aborting."; exit 1; }
+if [ -e /etc/motd ]; then cat /etc/motd; fi
+if [ -e /etc/update-motd.d ]; then run-parts /etc/update-motd.d/ 2>/dev/null; fi
+export SSHHOME=$(mktemp -d -t .$(whoami).sshrc.XXXX)
+export SSHRCCLEANUP=$SSHHOME
+trap "rm -rf $SSHRCCLEANUP; exit" 0
 
-            cat << 'EOF' > $SSHHOME/sshrc.bashrc
-            if [ -r /etc/profile ]; then source /etc/profile; fi
-            if [ -r ~/.bash_profile ]; then source ~/.bash_profile
-            elif [ -r ~/.bash_login ]; then source ~/.bash_login
-            elif [ -r ~/.profile ]; then source ~/.profile
-            fi
-            source $SSHHOME/.sshrc;
-            EOF
+cat << 'EOF' > $SSHHOME/sshrc.bashrc
+if [ -r /etc/profile ]; then source /etc/profile; fi
+if [ -r ~/.bash_profile ]; then source ~/.bash_profile
+elif [ -r ~/.bash_login ]; then source ~/.bash_login
+elif [ -r ~/.profile ]; then source ~/.profile
+fi
+source $SSHHOME/.sshrc;
+EOF
 
-            echo "{{.sshrc_content}}" | xxd -p -r | tar mxz -C $SSHHOME
+echo "{{.sshrc_content}}" | xxd -p -r | tar mxz -C $SSHHOME
 
-            bash --rcfile $SSHHOME/sshrc.bashrc
-            exit $?
-        ]=], dict)
+bash --rcfile $SSHHOME/sshrc.bashrc
+exit $?
+]=], dict)
 
         return severside_script
     end
