aa6311c8a2931bbc06ce5d4d67e667d1521c3efa
Kohki Makimoto <kohki.makimoto@gmail.com>
2016-11-26 19:13:33 +0900

    WIP: improve tests

--- essh/essh_test.go
+++ essh/essh_test.go
@@ -1,12 +1,12 @@
 package essh
 
 import (
+	"bytes"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
-	_ "bytes"
-	_ "io"
 )
 
 func TestShowSSHConfig(t *testing.T) {
@@ -30,47 +30,47 @@ func TestShowSSHConfig(t *testing.T) {
 	}
 }
 
-//func TestConnectHost(t *testing.T) {
-//	tmpDir, err := ioutil.TempDir("", "")
-//	if err != nil {
-//		t.Errorf("should not raise error: %v", err)
-//	}
-//	defer func() {
-//		os.RemoveAll(tmpDir)
-//	}()
-//	configFile := filepath.Join(tmpDir, "esshconfig.lua")
-//	if err = ioutil.WriteFile(configFile, []byte(testConfig), 0644); err != nil {
-//		t.Errorf("should not raise error: %v", err)
-//	}
-//	os.Chdir(tmpDir)
-//
-//	// capture stdout
-//	// borrowed from http://stackoverflow.com/questions/10473800/in-go-how-do-i-capture-stdout-of-a-function-into-a-string
-//	old := os.Stdout
-//	r, w, _ := os.Pipe()
-//	os.Stdout = w
-//	outC := make(chan string)
-//	go func() {
-//		var buf bytes.Buffer
-//		io.Copy(&buf, r)
-//		outC <- buf.String()
-//	}()
-//
-//	// test
-//	exitCode := Run([]string{"essh_test_ssh_server", "bash -c 'echo -n connected'"})
-//	if exitCode != 0 {
-//		t.Error("exited with non zero")
-//	}
-//
-//	// back to normal state
-//	w.Close()
-//	os.Stdout = old
-//	out := <-outC
-//
-//	if out != "connected" {
-//		t.Errorf("invalid output: %v", out)
-//	}
-//}
+func TestConnectHost(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Errorf("should not raise error: %v", err)
+	}
+	defer func() {
+		os.RemoveAll(tmpDir)
+	}()
+	configFile := filepath.Join(tmpDir, "esshconfig.lua")
+	if err = ioutil.WriteFile(configFile, []byte(testConfig), 0644); err != nil {
+		t.Errorf("should not raise error: %v", err)
+	}
+	os.Chdir(tmpDir)
+
+	// capture stdout
+	// borrowed from http://stackoverflow.com/questions/10473800/in-go-how-do-i-capture-stdout-of-a-function-into-a-string
+	old := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	outC := make(chan string)
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outC <- buf.String()
+	}()
+
+	// test
+	exitCode := Run([]string{"essh_test_ssh_server", "bash -c 'echo -n connected'"})
+	if exitCode != 0 {
+		t.Error("exited with non zero")
+	}
+
+	// back to normal state
+	w.Close()
+	os.Stdout = old
+	out := <-outC
+
+	if out != "connected" {
+		t.Errorf("invalid output: %v", out)
+	}
+}
 
 var testConfig string
 
