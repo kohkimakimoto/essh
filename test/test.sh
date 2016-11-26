4b15fcdcce811c3d8ce96f829744aaf13e0a879c
Kohki Makimoto <kohki.makimoto@gmail.com>
2016-11-26 11:15:56 +0900

    improve tests

---
+++ test/test.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -eu
+
+indent() {
+    local n="${1:-4}"
+    local p=""
+    for i in `seq 1 $n`; do
+        p="$p "
+    done;
+
+    local c="s/^/$p/"
+    case $(uname) in
+      Darwin) sed -l "$c";; # mac/bsd sed: -l buffers on line boundaries
+      *)      sed -u "$c";; # unix/gnu sed: -u unbuffered (arbitrary) chunks of data
+    esac
+}
+
+if [ "${TERM:-dumb}" != "dumb" ]; then
+    txtunderline=$(tput sgr 0 1)     # Underline
+    txtbold=$(tput bold)             # Bold
+    txtred=$(tput setaf 1)           # red
+    txtgreen=$(tput setaf 2)         # green
+    txtyellow=$(tput setaf 3)        # yellow
+    txtblue=$(tput setaf 4)          # blue
+    txtreset=$(tput sgr0)            # Reset
+else
+    txtunderline=""
+    txtbold=""
+    txtred=""
+    txtgreen=""
+    txtyellow=""
+    txtblue=$""
+    txtreset=""
+fi
+
+GOTEST_FLAGS=${GOTEST_FLAGS:--cover -timeout=360s}
+
+test_dir=$(cd $(dirname $0); pwd)
+cd "$test_dir/.."
+
+echo "--> Running tests (flags: $GOTEST_FLAGS)..."
+GOBIN="`which go`"
+$GOBIN test $GOTEST_FLAGS $($GOBIN list ./... | grep -v vendor) | indent
+
+echo "--> Done."
