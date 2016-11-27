82533a4f8376da114a537eb68ae1b90615f7e0a6
Kohki Makimoto <kohki.makimoto@gmail.com>
2016-11-27 13:01:48 +0900

    WIP: improve tests

--- essh/essh_test.go
+++ essh/essh_test.go
@@ -72,6 +72,50 @@ func TestRunSSH(t *testing.T) {
 	}
 }
 
+func TestRunExec(t *testing.T) {
+	tmpDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Errorf("should not raise error: %v", err)
+	}
+	defer func() {
+		os.RemoveAll(tmpDir)
+	}()
+	configFile := filepath.Join(tmpDir, "esshconfig.lua")
+	if err = ioutil.WriteFile(configFile, []byte(testConfig), 0644); err != nil {
+		t.Errorf("should not raise error: %v", err)
+	}
+	os.Chdir(tmpDir)
+
+	// capture stdout
+	// borrowed from http://stackoverflow.com/questions/10473800/in-go-how-do-i-capture-stdout-of-a-function-into-a-string
+	old := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	outC := make(chan string)
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outC <- buf.String()
+	}()
+
+	// test
+	exitCode := Run([]string{"--exec", "--target=foo", "--backend=remote", "--prefix", "echo foo"})
+	if exitCode != 0 {
+		t.Error("exited with non zero")
+	}
+
+	// back to normal state
+	w.Close()
+	os.Stdout = old
+	out := <-outC
+
+	if out != `[remote:essh_test_ssh_server1] foo
+[remote:essh_test_ssh_server2] foo
+` {
+		t.Errorf("invalid output: %v", out)
+	}
+}
+
 var testConfig string
 
 func init() {
